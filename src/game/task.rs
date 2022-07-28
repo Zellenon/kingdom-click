f6670078f885e5ee65bf4fb222f99a2aeec04f52
Zellenon <Zellenon@gmail.com>
2022-07-28 15:16:35 -0500

    task progress

--- src/game/task.rs
+++ src/game/task.rs
@@ -1,6 +1,19 @@
+use super::resource::ResourceModification;
 use bevy::prelude::*;
 
 pub struct Task {
     pub name: &'static str,
     pub description: &'static str,
+    pub completion_message: &'static str,
+    pub max_progress: f32,
+    pub progress: f32,
+    pub completion_outcome: TaskOutcome,
+}
+
+pub enum TaskOutcome {
+    ResourceOutcome(ResourceModification),
+}
+
+pub struct GodStats {
+    click_power: usize,
 }
